8139493e63acc50529b07d4e7ae63de0f5a6175e
SahilThakur7 <sahilrd07@gmail.com>
2018-08-04 17:04:14 +0530

    started routing

--- src/axios.js
+++ src/axios.js
@@ -4,3 +4,5 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 });
+
+export default instance;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,68 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Posts from './Posts/Posts';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import {Route} from 'react-router-dom';
+
 import './Blog.css';
 
 class Blog extends Component {
-
-    state ={
-        posts: [],
-        selectedPostId: null,
-        error: false
-
-    }
-
-    componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(
-            response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'KV'
-                    }
-                });
-                this.setState({
-                    posts: updatedPosts
-                })
-            }
-        ).catch(error=> {
-            this.setState({error: true})
-        });
-
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
-
-    }
-
     render () {
-
-        let posts = <p style={{textAlign: 'center'}}>This is not working!</p>
-            if(!this.state.error){
-                 posts = this.state.posts.map(post => {
-                    return <Post title={post.title} author={post.author} key={post.id}
-                        clicked={ () => this.postSelectedHandler(post.id)}/>
-                });
-            }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
